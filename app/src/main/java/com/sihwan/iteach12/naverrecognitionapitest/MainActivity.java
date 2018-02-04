95c5222e051399bf63d7b48c82530db5c0312a26
박상준 <iteach12@bagsangjun-ui-MacBook-Pro.local>
2018-02-04 14:15:52 +0900

    Initial commit

---
+++ app/src/main/java/com/sihwan/iteach12/naverrecognitionapitest/MainActivity.java
@@ -0,0 +1,328 @@
+package com.sihwan.iteach12.naverrecognitionapitest;
+
+import android.content.ContentValues;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import com.sihwan.iteach12.naverrecognitionapitest.utils.AudioWriterPCM;
+import com.naver.speech.clientapi.SpeechRecognitionResult;
+
+
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String CLIENT_ID = "MTaabvfKipKDh2clp5Xl";
+    // 1. "내 애플리케이션"에서 Client ID를 확인해서 이곳에 적어주세요.
+    // 2. build.gradle (Module:app)에서 패키지명을 실제 개발자센터 애플리케이션 설정의 '안드로이드 앱 패키지 이름'으로 바꿔 주세요
+
+    private RecognitionHandler handler;
+    private NaverRecognizer naverRecognizer;
+
+    private TextView txtResult;
+    private Button btnStart;
+    private String mResult;
+
+    private AudioWriterPCM writer;
+
+
+
+
+    //음성합성 관련
+    // CONNECTION_TIMEOUT and READ_TIMEOUT are in milliseconds
+    public static final int CONNECTION_TIMEOUT = 10000;
+    public static final int READ_TIMEOUT = 15000;
+
+    //내가 쓴 텍스트를 음성합성
+    EditText myText;
+    Button play_mp3;
+    SeekBar seekBar;
+    //php에 접속한 결과를 가져온 스트링
+    String result_from_php;
+
+
+
+    //contentValues관련 변수들
+    String user_name = "iteach12";
+    String user_text;
+    String user_voice = "mijin";
+    int user_speed = 0;
+    //음성합성 관련
+
+    // Handle speech recognition Messages.
+    private void handleMessage(Message msg) {
+        switch (msg.what) {
+            case R.id.clientReady:
+                // Now an user can speak.
+                txtResult.setText("Connected");
+                writer = new AudioWriterPCM(
+                        Environment.getExternalStorageDirectory().getAbsolutePath() + "/NaverSpeechTest");
+                writer.open("Test");
+                break;
+
+            case R.id.audioRecording:
+                writer.write((short[]) msg.obj);
+                break;
+
+            case R.id.partialResult:
+                // Extract obj property typed with String.
+                mResult = (String) (msg.obj);
+                txtResult.setText(mResult);
+                break;
+
+            case R.id.finalResult:
+                // Extract obj property typed with String array.
+                // The first element is recognition result for speech.
+                SpeechRecognitionResult speechRecognitionResult = (SpeechRecognitionResult) msg.obj;
+                List<String> results = speechRecognitionResult.getResults();
+                StringBuilder strBuf = new StringBuilder();
+                for(String result : results) {
+                    strBuf.append(result);
+                    strBuf.append("\n");
+                }
+                mResult = strBuf.toString();
+                txtResult.setText(mResult);
+                break;
+
+            case R.id.recognitionError:
+                if (writer != null) {
+                    writer.close();
+                }
+
+                mResult = "Error code : " + msg.obj.toString();
+                txtResult.setText(mResult);
+                btnStart.setText(R.string.str_start);
+                btnStart.setEnabled(true);
+                break;
+
+            case R.id.clientInactive:
+                if (writer != null) {
+                    writer.close();
+                }
+
+                btnStart.setText(R.string.str_start);
+                btnStart.setEnabled(true);
+                break;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+        //배경화면
+//        CoordinatorLayout constraintLayout = (CoordinatorLayout) findViewById(R.id.basic_layout);
+//        constraintLayout.setBackgroundResource(R.drawable.house_back_basic);
+
+        //배경화면
+
+
+
+
+        txtResult = (TextView) findViewById(R.id.txt_result);
+        btnStart = (Button) findViewById(R.id.btn_start);
+
+        handler = new RecognitionHandler(this);
+        naverRecognizer = new NaverRecognizer(this, handler, CLIENT_ID);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if(!naverRecognizer.getSpeechRecognizer().isRunning()) {
+                    // Start button is pushed when SpeechRecognizer's state is inactive.
+                    // Run SpeechRecongizer by calling recognize().
+                    mResult = "";
+                    txtResult.setText("Connecting...");
+                    btnStart.setText(R.string.str_stop);
+                    naverRecognizer.recognize();
+                } else {
+                    Log.d(TAG, "stop and wait Final Result");
+                    btnStart.setEnabled(false);
+
+                    naverRecognizer.getSpeechRecognizer().stop();
+                }
+            }
+        });
+
+
+
+
+        ////////음성합성관련
+        seekBar = (SeekBar)findViewById(R.id.seekBar);
+        seekBar.setProgress(0);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                String speed_current;
+                user_speed = seekBar.getProgress();
+                speed_current = String.valueOf(user_speed);
+                Log.i("seek bar", speed_current);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        findViewById(R.id.sysn_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                new NaverSysnthesisAsynkTask().execute();
+
+                // URL 설정.
+                String url = "http://lovepusa.cafe24.com/naverapi.php";
+
+                // AsyncTask를 통해 HttpURLConnection 수행.
+
+                user_text=myText.getText().toString();
+
+                NetworkTask networkTask = new NetworkTask(url, setContentsValues(user_name, user_text, user_voice, user_speed));
+                networkTask.execute();
+                Log.i("Result", setContentsValues(user_name, user_text, user_voice, user_speed).toString());
+
+
+            }
+        });
+
+
+        play_mp3 = (Button)findViewById(R.id.sysn_play);
+        play_mp3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                MediaPlayer mediaPlayer = new MediaPlayer();
+
+                try {
+                    mediaPlayer.setDataSource("http://lovepusa.cafe24.com/iteach12.mp3");
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        myText = (EditText)findViewById(R.id.sysn_textView);
+
+
+        ////////음성합성관련
+
+
+
+
+}
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // NOTE : initialize() must be called on start time.
+        naverRecognizer.getSpeechRecognizer().initialize();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mResult = "";
+        txtResult.setText("");
+        btnStart.setText(R.string.str_start);
+        btnStart.setEnabled(true);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // NOTE : release() must be called on stop time.
+        naverRecognizer.getSpeechRecognizer().release();
+    }
+
+    // Declare handler for handling SpeechRecognizer thread's Messages.
+    static class RecognitionHandler extends Handler {
+        private final WeakReference<MainActivity> mActivity;
+
+        RecognitionHandler(MainActivity activity) {
+            mActivity = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            MainActivity activity = mActivity.get();
+            if (activity != null) {
+                activity.handleMessage(msg);
+            }
+        }
+    }
+
+
+
+    //////////////////음성 합성 관련 /////////////////////
+    public class NetworkTask extends AsyncTask<Void, Void, String> {
+
+        private String url;
+        private ContentValues values;
+
+        public NetworkTask(String url, ContentValues values) {
+
+            this.url = url;
+            this.values = values;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            String result; // 요청 결과를 저장할 변수.
+            RequestHttpUrlConnection requestHttpURLConnection = new RequestHttpUrlConnection();
+            result = requestHttpURLConnection.request(url, values); // 해당 URL로 부터 결과물을 얻어온다.
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            //doInBackground()로 부터 리턴된 값이 onPostExecute()의 매개변수로 넘어오므로 s를 출력한다.
+
+            Log.i("Result of PHP", s);
+        }
+    }
+
+
+    public ContentValues setContentsValues(String name, String my_text, String voice, int speed){
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", name);
+        contentValues.put("text", my_text);
+        contentValues.put("voice", voice);
+        contentValues.put("speed", speed);
+        return contentValues;
+    }
+    //////////////////음성 합성 관련 /////////////////////
+}
