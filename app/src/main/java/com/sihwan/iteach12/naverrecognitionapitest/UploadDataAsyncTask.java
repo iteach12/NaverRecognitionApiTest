9522d1ffa85957e9126643fbfc9633fc5943c67d
iteach12 <iteach12@gmail.com>
2018-02-23 12:41:12 +0900

    UploadDataAsyncTask.java
    데이터 업로드용 어싱크태스크
    
    아마도 doInBackground의 파라미터를 맵으로 넣어야 할듯
    가변 파라미터 ...의 의미 공부
    
    되도록 어싱크 하나로 쓰고 싶고. 연속으로 불렀을 때의 메모리 관련 확인해보자..시간이되면

---
+++ app/src/main/java/com/sihwan/iteach12/naverrecognitionapitest/UploadDataAsyncTask.java
@@ -0,0 +1,17 @@
+package com.sihwan.iteach12.naverrecognitionapitest;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by iteach12 on 2018. 2. 23..
+ * AsyncTask는 메인 UI에서 불러와야 한다.
+ * ...은 가변 파라미터 (배열, 길이의 변동이 있는 것?)
+ *
+ */
+
+public class UploadDataAsyncTask extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+}
