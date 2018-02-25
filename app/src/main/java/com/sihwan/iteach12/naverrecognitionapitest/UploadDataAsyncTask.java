cabb2e83e8a49ce134120d90210c0bbc590ac180
iteach12 <iteach12@gmail.com>
2018-02-25 22:58:23 +0900

    dev_firebase 브랜치 테스트 222

--- app/src/main/java/com/sihwan/iteach12/naverrecognitionapitest/UploadDataAsyncTask.java
+++ app/src/main/java/com/sihwan/iteach12/naverrecognitionapitest/UploadDataAsyncTask.java
@@ -15,4 +15,11 @@ public class UploadDataAsyncTask extends AsyncTask {
         return null;
 
     }
+
+
+    private void dev_merge_test(){
+
+        //머지 테스트 데브 브랜치에서 머지를 하면 어디로 가는가??
+    }
+
 }
