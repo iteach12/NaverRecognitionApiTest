7888cb77e9cefee3f8d8a0b117817f46c9ce6d8f
iteach12 <iteach12@gmail.com>
2018-03-01 22:59:38 +0900

    firebase 트랜잭션 트랜잭션 = 하나의 데이터를 둘 이상이 동시에 접근하면 충돌이 생기기 때문에, 어느 한 쪽만 수정 가능하도록 조절해주는 장치?? 주로 좋아요 누를 때... 사용하는
    암튼 좋아요 눌러주는 방법 완료.

--- app/src/main/java/com/sihwan/iteach12/naverrecognitionapitest/BoardActivity.java
+++ app/src/main/java/com/sihwan/iteach12/naverrecognitionapitest/BoardActivity.java
@@ -1,6 +1,7 @@
 package com.sihwan.iteach12.naverrecognitionapitest;
 
 
+import android.provider.ContactsContract;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,13 +10,18 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
 import com.google.firebase.database.ValueEventListener;
 
 
@@ -30,12 +36,17 @@ public class BoardActivity extends AppCompatActivity {
     private List<String> uidLists = new ArrayList<>();
     private FirebaseDatabase database;
     private BoardRecyclerViewAdapter boardRecyclerViewAdapter;
+    private FirebaseAuth auth;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
         database = FirebaseDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
 
         recyclerView = (RecyclerView)findViewById(R.id.recycleView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -46,9 +57,15 @@ public class BoardActivity extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
 
                 imageDTOs.clear();
+                uidLists.clear();
                 for(DataSnapshot snapshot : dataSnapshot.getChildren()){
                     ImageDTO imageDTO = snapshot.getValue(ImageDTO.class);
+                    String uidKey = snapshot.getKey();
                     imageDTOs.add(imageDTO);
+                    uidLists.add(uidKey);
+
+
+
                 }
                 boardRecyclerViewAdapter.notifyDataSetChanged();
 
@@ -66,6 +83,10 @@ public class BoardActivity extends AppCompatActivity {
 
     class BoardRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
 
+
+
+
+
         @Override
         public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
@@ -76,18 +97,68 @@ public class BoardActivity extends AppCompatActivity {
         }
 
         @Override
-        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
 
             ((CustomViewHolder)holder).textView1.setText(imageDTOs.get(position).title);
             ((CustomViewHolder)holder).textView2.setText(imageDTOs.get(position).description);
 
             Glide.with(holder.itemView.getContext()).load(imageDTOs.get(position).imageUrl).into(((CustomViewHolder)holder).imageView);
+
+            ((CustomViewHolder)holder).starButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onStarClicked(database.getReference().child("images").child(uidLists.get(position)));
+                }
+            });
+
+            if(imageDTOs.get(position).stars.containsKey(auth.getCurrentUser().getUid())){
+                ((CustomViewHolder)holder).starButton.setImageResource(R.drawable.ic_favorite_black_24dp);
+
+            }else {
+                ((CustomViewHolder)holder).starButton.setImageResource(R.drawable.ic_favorite_border_black_24dp);
+            }
+
+
         }
 
         @Override
         public int getItemCount() {
             return imageDTOs.size();
         }
+
+        private void onStarClicked(DatabaseReference postRef){
+
+            postRef.runTransaction(new Transaction.Handler() {
+                @Override
+                public Transaction.Result doTransaction(MutableData mutableData) {
+                    ImageDTO imageDTO = mutableData.getValue(ImageDTO.class);
+                    if (imageDTO == null) {
+                        mutableData.setValue(imageDTO);
+                    }
+                    if(imageDTO.stars.containsKey(auth.getCurrentUser().getUid())) {
+
+                        imageDTO.starCount = imageDTO.starCount - 1;
+                        imageDTO.stars.remove(auth.getCurrentUser().getUid());
+
+                    } else {
+                        imageDTO.starCount = imageDTO.starCount + 1;
+                        imageDTO.stars.put(auth.getCurrentUser().getUid(), true);
+                    }
+
+                    mutableData.setValue(imageDTO);
+                    return Transaction.success(mutableData);
+                }
+
+                @Override
+                public void onComplete(DatabaseError databaseError, boolean committed, DataSnapshot dataSnapshot) {
+                    System.out.println("Transaction completed");
+                }
+            });
+        }
+
+
+
+
     }
 
     private class CustomViewHolder extends RecyclerView.ViewHolder{
@@ -95,6 +166,7 @@ public class BoardActivity extends AppCompatActivity {
         ImageView imageView;
         TextView textView1;
         TextView textView2;
+        ImageView starButton;
 
 
         public CustomViewHolder(View itemView) {
@@ -103,6 +175,7 @@ public class BoardActivity extends AppCompatActivity {
             imageView = (ImageView)itemView.findViewById(R.id.item_imageView);
             textView1 = (TextView)itemView.findViewById(R.id.item_textView1);
             textView2 = (TextView)itemView.findViewById(R.id.item_textView2);
+            starButton = (ImageView)itemView.findViewById(R.id.item_starButton_imageView);
 
 
         }
--- app/src/main/java/com/sihwan/iteach12/naverrecognitionapitest/ImageDTO.java
+++ app/src/main/java/com/sihwan/iteach12/naverrecognitionapitest/ImageDTO.java
@@ -1,5 +1,8 @@
 package com.sihwan.iteach12.naverrecognitionapitest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by iteach12 on 2018. 3. 1..
  */
@@ -11,4 +14,6 @@ public class ImageDTO {
     public String description;
     public String uid;
     public String userId;
+    public int starCount = 0;
+    public Map<String, Boolean> stars = new HashMap<>();
 }
